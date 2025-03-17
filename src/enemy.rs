40f8c224c239337a95613bb3ec09d90b1a814cef
Miguel Palhas <mpalhas@gmail.com>
2025-03-17 15:17:22 +0000

    wip

---
+++ src/enemy.rs
@@ -0,0 +1,53 @@
+use crate::constants::*;
+use bevy::prelude::*;
+
+#[derive(Component, Default)]
+pub struct Enemy;
+
+#[derive(Bundle, Default)]
+pub struct EnemyBundle {
+    enemy: Enemy,
+    sprite: Sprite,
+    transform: Transform,
+}
+
+impl EnemyBundle {
+    pub fn new(location: Vec2, size: Vec2) -> Self {
+        Self {
+            sprite: Sprite {
+                color: ENEMY_COLOR,
+                custom_size: Some(size),
+                ..default()
+            },
+            transform: Transform::from_translation(location.extend(0.0)),
+            ..default()
+        }
+    }
+}
+
+#[derive(Debug, Default, Resource, PartialEq, Eq)]
+pub enum EnemyDirection {
+    #[default]
+    Right,
+    Left,
+}
+
+#[derive(Event, Default)]
+pub struct EnemyDirectionChanged;
+
+impl EnemyDirection {
+    pub fn reverse(&mut self) {
+        if *self == EnemyDirection::Right {
+            *self = EnemyDirection::Left;
+        } else {
+            *self = EnemyDirection::Right;
+        }
+    }
+
+    pub fn as_f32(&self) -> f32 {
+        match self {
+            EnemyDirection::Right => 1.0,
+            EnemyDirection::Left => -1.0,
+        }
+    }
+}
